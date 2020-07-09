be967d7e3c409b77c7096a9234f05bcac3e2e252
WERY Adrien <adrien@wery.io>
2020-07-09 19:51:37 +0200

    Initial Commit

---
+++ latest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  latest
+//
+//  Created by Adrien WERY on 7/9/20.
+//  Copyright © 2020 Adrien WERY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
